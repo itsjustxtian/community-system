46d1ea5d703665858863a85db5a1bb444528ce09
oconchristiane <ocon.christiane@gmail.com>
2025-02-12 16:54:58 +0800

    [itsjustxtian] tried and still trying to add shadcn ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,10 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), icon()], 
+  integrations: [
+    react(), 
+    icon(),
+  ], 
 
   vite: {
     plugins: [tailwindcss()]
